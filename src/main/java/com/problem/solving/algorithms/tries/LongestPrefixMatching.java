eac3a8b380aabcc42541da457bf15628c76b00eb
Vijay Sidhu <vijaysidhu@gmail.com>
2020-11-01 19:45:45 -0800

    Longest Prefix matching

--- src/main/java/com/problem/solving/algorithms/tries/LongestPrefixMatching.java
+++ src/main/java/com/problem/solving/algorithms/tries/LongestPrefixMatching.java
@@ -1,5 +1,7 @@
 package com.problem.solving.algorithms.tries;
 
+import java.util.Map;
+
 /**
  * Given a dictionary of words and an input string, find the longest prefix of the string which is
  * also a word in dictionary. Examples:
@@ -13,14 +15,86 @@ package com.problem.solving.algorithms.tries;
  */
 public class LongestPrefixMatching {
 
-/**
- * https://www.geeksforgeeks.org/longest-prefix-matching-a-trie-based-solution-in-java/
- * We build a Trie of all dictionary words.
- * Once the Trie is built, traverse through it using
- * characters of input string. If prefix matches a dictionary word,
- * store current length and look for a longer match.
- * Finally, return the longest match.
- */
+  /**
+   * https://www.geeksforgeeks.org/longest-prefix-matching-a-trie-based-solution-in-java/ We build a
+   * Trie of all dictionary words. Once the Trie is built, traverse through it using characters of
+   * input string. If prefix matches a dictionary word, store current length and look for a longer
+   * match. Finally, return the longest match.
+   */
 
 
+  public static void main(String[] args) {
+    Trie dict = new Trie();
+    dict.insertRecursive("are");
+    dict.insertRecursive("area");
+    dict.insertRecursive("base");
+    dict.insertRecursive("cat");
+    dict.insertRecursive("cater");
+    dict.insertRecursive("basement");
+    String input = "caterer";
+    System.out.print(input + ":   ");
+    System.out.println(getMatchingPrefix(dict.getRoot(), input));
+
+    input = "basement";
+    System.out.print(input + ":   ");
+    System.out.println(getMatchingPrefix(dict.getRoot(), input));
+
+    input = "are";
+    System.out.print(input + ":   ");
+    System.out.println(getMatchingPrefix(dict.getRoot(), input));
+
+    input = "arex";
+    System.out.print(input + ":   ");
+    System.out.println(getMatchingPrefix(dict.getRoot(), input));
+
+    input = "basemexz";
+    System.out.print(input + ":   ");
+    System.out.println(getMatchingPrefix(dict.getRoot(), input));
+
+    input = "xyz";
+    System.out.print(input + ":   ");
+    System.out.println(getMatchingPrefix(dict.getRoot(), input));
+
+  }
+
+  // The main method that finds out the longest string 'input'
+  public static String getMatchingPrefix(TrieNode root, String input) {
+    String result = ""; // Initialize resultant string
+    int length = input.length();  // Find length of the input string
+
+    // Initialize reference to traverse through Trie
+    TrieNode crawl = root;
+
+    // Iterate through all characters of input string 'str' and traverse
+    // down the Trie
+    int level, prevMatch = 0;
+    for (level = 0; level < length; level++) {
+      // Find current character of str
+      char ch = input.charAt(level);
+
+      // HashMap of current Trie node to traverse down
+      Map<Character, TrieNode> child = crawl.getChildren();
+
+      // See if there is a Trie edge for the current character
+      if (child.containsKey(ch)) {
+        result += ch;          //Update result
+        crawl = child.get(ch); //Update crawl to move down in Trie
+
+        // If this is end of a word, then update prevMatch
+        if (crawl.isEndOfWord()) {
+          prevMatch = level + 1;
+        }
+      } else {
+        break;
+      }
+    }
+
+    // If the last processed character did not match end of a word,
+    // return the previously matching prefix
+    if (!crawl.isEndOfWord()) {
+      return result.substring(0, prevMatch);
+    } else {
+      return result;
+    }
+  }
 }
--- src/main/java/com/problem/solving/algorithms/tries/Trie.java
+++ src/main/java/com/problem/solving/algorithms/tries/Trie.java
@@ -8,53 +8,100 @@ public class Trie {
     root = new TrieNode();
   }
 
-  /**
-   * Insert character into trie Search recursive O(Key_Lengh) Memory requirements
-   * O(ALPHABET_SIZE*key_length*N)
-   */
-  public void insertRecursive(TrieNode current, String word, int index) {
+  public TrieNode getRoot() {
+    return root;
+  }
 
+  /**
+   * Iterative implementation of insert into trie
+   */
+  public void insert(String word) {
+    TrieNode current = root;
+    for (int i = 0; i < word.length(); i++) {
+      char ch = word.charAt(i);
+      TrieNode node = current.getChildren().get(ch);
+      if (node == null) {
+        node = new TrieNode();
+        current.getChildren().put(ch, node);
+      }
+      current = node;
+    }
+    //mark the current nodes endOfWord as true
+    current.setEndOfWord(true);
+  }
+
+  /**
+   * Recursive implementation of insert into trie
+   */
+  public void insertRecursive(String word) {
+    insertRecursive(root, word, 0);
+  }
+
+  private void insertRecursive(TrieNode current, String word, int index) {
     if (index == word.length()) {
+      //if end of word is reached then mark endOfWord as true on current node
       current.setEndOfWord(true);
       return;
     }
+    char ch = word.charAt(index);
+    TrieNode node = current.getChildren().get(ch);
 
-    char currentChar = word.charAt(index);
-
-    // Check if current char is in trie
-    TrieNode node = current.getChildren().get(currentChar);
-
-    // If current character is not in trie create node
+    //if node does not exists in map then create one and put it into map
     if (node == null) {
       node = new TrieNode();
-      node.getChildren().put(currentChar, node);
+      current.getChildren().put(ch, node);
     }
-
-    // If current character is in trie repeat the abpve steps
     insertRecursive(node, word, index + 1);
   }
 
   /**
-   * Search recursive O(Key_Lengh) Memory requirements O(ALPHABET_SIZE*key_length*N)
+   * Iterative implementation of search into trie.
    */
-  private boolean searchRecursive(TrieNode current, String word, int index) {
+  public boolean search(String word) {
+    TrieNode current = root;
+    for (int i = 0; i < word.length(); i++) {
+      char ch = word.charAt(i);
+      TrieNode node = current.getChildren().get(ch);
+      //if node does not exist for given char then return false
+      if (node == null) {
+        return false;
+      }
+      current = node;
+    }
+    //return true of current's endOfWord is true else return false.
+    return current.isEndOfWord();
+  }
 
+  /**
+   * Recursive implementation of search into trie.
+   */
+  public boolean searchRecursive(String word) {
+    return searchRecursive(root, word, 0);
+  }
+
+  private boolean searchRecursive(TrieNode current, String word, int index) {
     if (index == word.length()) {
+      //return true of current's endOfWord is true else return false.
       return current.isEndOfWord();
     }
     char ch = word.charAt(index);
     TrieNode node = current.getChildren().get(ch);
+    //if node does not exist for given char then return false
     if (node == null) {
       return false;
     }
     return searchRecursive(node, word, index + 1);
   }
 
-  /*
-  Key may not be there in trie. Delete operation should not modify trie.
-  Key present as unique key (no part of key contains another key (prefix), nor the key itself is prefix of another key in trie). Delete all the nodes.
-  Key is prefix key of another long key in trie. Unmark the leaf node.
-  Key present in trie, having atleast one other key as prefix key. Delete nodes from end of key until first leaf node of longest prefix key.
+  /**
+   * Delete word from trie.
+   */
+  public void delete(String word) {
+    delete(root, word, 0);
+  }
+
+  /**
+   * Returns true if parent should delete the mapping
    */
   private boolean delete(TrieNode current, String word, int index) {
     if (index == word.length()) {
@@ -81,6 +128,5 @@ public class Trie {
     }
     return false;
   }
-
 
 }
