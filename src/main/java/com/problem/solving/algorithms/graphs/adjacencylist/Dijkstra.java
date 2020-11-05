5f0e356f554f289f561f54980d2180c08addc0bd
Vijay Sidhu <vijaysidhu@gmail.com>
2020-11-05 13:53:52 -0800

    dijkstra

--- src/main/java/com/problem/solving/algorithms/graphs/adjacencylist/Dijkstra.java
+++ src/main/java/com/problem/solving/algorithms/graphs/adjacencylist/Dijkstra.java
@@ -6,6 +6,13 @@ import java.util.List;
 import java.util.PriorityQueue;
 import javafx.util.Pair;
 
+/*
+Mark your selected initial node with a current distance of 0 and the rest with infinity.
+Set the non-visited node with the smallest current distance as the current node C.
+For each neighbour N of your current node C: add the current distance of C with the weight of the edge connecting C-N. If it's smaller than the current distance of N, set it as the new current distance of N.
+Mark the current node C as visited.
+If there are non-visited nodes, go to step 2.
+ */
 public class Dijkstra {
 
   private int numberOfVertices;
