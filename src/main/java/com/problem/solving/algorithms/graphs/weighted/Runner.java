a60fe069bfb042e8625e2053e2793d57692ccf80
Vijay Sidhu <vijaysidhu@gmail.com>
2020-11-08 11:20:35 -0800

    Kruksal

--- src/main/java/com/problem/solving/algorithms/graphs/weighted/Runner.java
+++ src/main/java/com/problem/solving/algorithms/graphs/weighted/Runner.java
@@ -6,25 +6,24 @@ import java.util.List;
 public class Runner {
 
   public static void main(String[] args) {
-    Vertex v1 = new Vertex("A");
+   /* Vertex v1 = new Vertex("A");
     Vertex v2 = new Vertex("B");
     Vertex v3 = new Vertex("C");
 
     v1.addNeighbour(new Edge(1, v1, v2));
-    v1.addNeighbour(new Edge(10, v1, v2));
-
-    v2.addNeighbour(new Edge(1, v2, v3));
+    v1.addNeighbour(new Edge(1, v1, v3));
+    v2.addNeighbour(new Edge(1, v2, v3));*/
 
     // Bellman ford
     System.out.println("Bellman ford");
     Vertex vertexA = new Vertex("A");
     Vertex vertexB = new Vertex("B");
     Vertex vertexC = new Vertex("C");
-    Vertex vertexD = new Vertex("D");
-    Vertex vertexE = new Vertex("E");
-    Edge edgeA = new Edge(1, vertexB, vertexC);
+
+    Edge edgeA = new Edge(1, vertexA, vertexB);
     Edge edgeB = new Edge(1, vertexA, vertexC);
-    Edge edgeC = new Edge(1, vertexA, vertexB);
+    Edge edgeC = new Edge(1, vertexB, vertexC);
+
     vertexA.addNeighbour(edgeB);
     vertexA.addNeighbour(edgeC);
     vertexB.addNeighbour(edgeA);
