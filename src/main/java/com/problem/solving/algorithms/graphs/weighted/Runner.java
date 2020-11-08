551bec9a3e4324df75cc5ae7e340e52852285113
Vijay Sidhu <vijaysidhu@gmail.com>
2020-11-08 11:30:33 -0800

    Kruksal

--- src/main/java/com/problem/solving/algorithms/graphs/weighted/Runner.java
+++ src/main/java/com/problem/solving/algorithms/graphs/weighted/Runner.java
@@ -19,20 +19,25 @@ public class Runner {
     Vertex vertexA = new Vertex("A");
     Vertex vertexB = new Vertex("B");
     Vertex vertexC = new Vertex("C");
-
-    Edge edgeA = new Edge(1, vertexA, vertexB);
-    Edge edgeB = new Edge(1, vertexA, vertexC);
-    Edge edgeC = new Edge(1, vertexB, vertexC);
-
-    vertexA.addNeighbour(edgeB);
+    Vertex vertexD = new Vertex("D");
+    Vertex vertexE = new Vertex("E");
+    Edge edgeAB = new Edge(-1, vertexA, vertexB);
+    Edge edgeAC = new Edge(4, vertexA, vertexC);
+    Edge edgeBC = new Edge(3, vertexB, vertexC);
+    Edge edgeBD = new Edge(2, vertexB, vertexD);
+    Edge edgeBE = new Edge(2, vertexB, vertexE);
+    Edge edgeDC = new Edge(5, vertexD, vertexC);
+    Edge edgeDB = new Edge(2, vertexD, vertexB);
+    Edge edgeED = new Edge(-3, vertexE, vertexD);
+   /* vertexA.addNeighbour(edgeB);
     vertexA.addNeighbour(edgeC);
     vertexB.addNeighbour(edgeA);
     vertexB.addNeighbour(edgeC);
     vertexC.addNeighbour(edgeA);
-    vertexC.addNeighbour(edgeB);
-
-    List<Vertex> vertices = Arrays.asList(vertexA, vertexB, vertexC);
-    List<Edge> edges = Arrays.asList(edgeA, edgeB, edgeC);
+    vertexC.addNeighbour(edgeB);*/
+    List<Vertex> vertices = Arrays.asList(vertexA, vertexB, vertexC, vertexD, vertexE);
+    List<Edge> edges = Arrays
+        .asList(edgeAB, edgeAC, edgeBC, edgeBD, edgeBE, edgeDC, edgeBD, edgeDB, edgeED);
 
     BellmanFord bmf = new BellmanFord(vertices, edges);
     bmf.shortestPath(vertexA, vertexC);
