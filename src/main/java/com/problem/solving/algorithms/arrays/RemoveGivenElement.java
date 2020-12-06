afcf580df1caaa2843835e575eb1818ba31aec88
Vijay Sidhu <vijaysidhu@gmail.com>
2020-12-05 21:34:41 -0800

    reverse k group

--- src/main/java/com/problem/solving/algorithms/arrays/RemoveGivenElement.java
+++ src/main/java/com/problem/solving/algorithms/arrays/RemoveGivenElement.java
@@ -1,25 +1,26 @@
 package com.problem.solving.algorithms.arrays;
 
-public class RemoveDuplicateFromSortedArray {
+public class RemoveGivenElement {
 
     public static void main(String[] args) {
         int[] array = new int[]{1, 1, 2, 3, 4, 4, 5, 6};
-        System.out.println(removeDuplicateSorted(array));
+        System.out.println(removeKSorted(array, 4));
     }
 
     /*
+    TC O(N)
       O(1) extra memory
      */
 
-    public static int removeDuplicateSorted(int[] array) {
+    public static int removeKSorted(int[] array, int k) {
         int len = array.length;
         int i = 0;
         for (int j = 1; j < len; j++) {
-            if (array[j] != array[i]) {
+            if (array[j] != k) {
                 i++;
                 array[i] = array[j];
             }
         }
-        return i + 1;
+        return i;
     }
 }
