6aae60aa354eeb9facb7c214c7c0efe8188d5d5a
Vijay Sidhu <vijaysidhu@gmail.com>
2020-12-07 13:17:51 -0800

    combination sum

--- src/main/java/com/problem/solving/algorithms/arrays/CombinationSum.java
+++ src/main/java/com/problem/solving/algorithms/arrays/CombinationSum.java
@@ -10,6 +10,17 @@ import java.util.List;
  */
 public class CombinationSum {
 
+    public static void main(String[] args) {
+        int[] arr = new int[]{10, 1, 2, 7, 6, 1, 5};
+        List<List<Integer>> ls = combinationSum2(arr, 8);
+        ls.stream().forEach(s -> {
+            s.stream().forEach(a -> {
+                System.out.print(a + " ");
+            });
+            System.out.println(" ");
+        });
+    }
+
     /*
     Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
 
@@ -17,7 +28,7 @@ public class CombinationSum {
 
     Note: The solution set must not contain duplicate combinations.
      */
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> results = new ArrayList<>();
 
         if (candidates == null || candidates.length == 0) {
@@ -32,7 +43,7 @@ public class CombinationSum {
         return results;
     }
 
-    private void toFindCombinationsToTarget(int[] candidates, List<List<Integer>> results, List<Integer> combination, int startIndex, int target) {
+    private static void toFindCombinationsToTarget(int[] candidates, List<List<Integer>> results, List<Integer> combination, int startIndex, int target) {
         if (target == 0) {
             results.add(new ArrayList<>(combination));
             return;
@@ -51,5 +62,20 @@ public class CombinationSum {
             toFindCombinationsToTarget(candidates, results, combination, i + 1, target - candidates[i]);
             combination.remove(combination.size() - 1);
         }
+    }
+
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i - nums[j] >= 0) {
+                    dp[i] += dp[i - nums[j]];
+                }
+            }
+        }
+
+        return dp[target];
     }
 }
