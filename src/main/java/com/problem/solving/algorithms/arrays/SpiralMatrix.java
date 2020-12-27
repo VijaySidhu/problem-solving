cb9c4d277a8fa231e4c14369fd46cfa14f35b1ce
Vijay Sidhu <vijaysidhu@gmail.com>
2020-12-26 18:44:52 -0800

    zigzag traverse of an array

--- src/main/java/com/problem/solving/algorithms/arrays/SpiralMatrix.java
+++ src/main/java/com/problem/solving/algorithms/arrays/SpiralMatrix.java
@@ -32,27 +32,32 @@ Space Complexity:
         List ans = new ArrayList();
         if (matrix.length == 0)
             return ans;
-        int r1 = 0, r2 = matrix.length - 1;
-        int c1 = 0, c2 = matrix[0].length - 1;
-        while (r1 <= r2 && c1 <= c2) {
-            for (int c = c1; c <= c2; c++) {
-                ans.add(matrix[r1][c]);
+        int startRow = 0, endRow = matrix.length - 1;
+        int startColumn = 0, endColumn = matrix[0].length - 1;
+        while (startRow <= endRow && startColumn <= endColumn) {
+            // Left to right
+            for (int c = startColumn; c <= endColumn; c++) {
+                ans.add(matrix[startRow][c]);
             }
-            for (int r = r1 + 1; r <= r2; r++) {
-                ans.add(matrix[r][c2]);
+            // top  to bottom
+            for (int r = startRow + 1; r <= endRow; r++) {
+                ans.add(matrix[r][endColumn]);
             }
-            if (r1 < r2 && c1 < c2) {
-                for (int c = c2 - 1; c > c1; c--) {
-                    ans.add(matrix[r2][c]);
+
+            if (startRow < endRow && startColumn < endColumn) {
+                // right to left
+                for (int c = endColumn - 1; c > startColumn; c--) {
+                    ans.add(matrix[endRow][c]);
                 }
-                for (int r = r2; r > r1; r--) {
-                    ans.add(matrix[r][c1]);
+                // bottom to top
+                for (int r = endRow; r > startRow; r--) {
+                    ans.add(matrix[r][startColumn]);
                 }
             }
-            r1++;
-            r2--;
-            c1++;
-            c2--;
+            startRow++;
+            endRow--;
+            startColumn++;
+            endColumn--;
         }
         return ans;
     }
