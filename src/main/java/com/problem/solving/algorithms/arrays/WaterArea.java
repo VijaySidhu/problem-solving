1bdba7db250252739b8d39221b9d4217430a2f6d
Vijay Sidhu <vijaysidhu@gmail.com>
2020-12-30 14:48:06 -0800

    rain water trap

--- src/main/java/com/problem/solving/algorithms/arrays/WaterArea.java
+++ src/main/java/com/problem/solving/algorithms/arrays/WaterArea.java
@@ -2,9 +2,16 @@ package com.problem.solving.algorithms.arrays;
 
 public class WaterArea {
 
-    // How much water is contained for each index ?
-    // find tallest pill to left and right of index
-    // take min height from two pillars and check what is height of index
+    /*
+       1. Take two pointers. First pointer points at starting position of an array and second one points at last element
+       2. Iterate an array and do the following
+            i.  Find maximum height left bar
+            ii. Find maximum height right bar
+                 If left bar height is less than right bar height increment left pointer and calculate area
+                 ELSE decrement right index and calculate water area
+       3. Return calculated water area
+     */
+    // TC O(n) SC O(1)
 
     public static int waterArea(int[] height) {
         if (height.length == 0) {
