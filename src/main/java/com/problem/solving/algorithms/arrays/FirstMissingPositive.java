34e8dc856f89bfd981391fe4faefa4371ee2eeb8
Vijay Sidhu <vijaysidhu@gmail.com>
2021-01-02 21:24:31 -0800

    Sudoku Solver

--- src/main/java/com/problem/solving/algorithms/arrays/FirstMissingPositive.java
+++ src/main/java/com/problem/solving/algorithms/arrays/FirstMissingPositive.java
@@ -2,42 +2,78 @@ package com.problem.solving.algorithms.arrays;
 
 public class FirstMissingPositive {
 
-    // Naive approach sort and scan TC O(nLogN) and SC O(1)
-    // Second use hashmap to restore number and its occurrence then scan array and find missing positive SC O(n)
-
-    // Put each number in its right place first missing positive must be within [1,index+1]
-    /*
-    The main idea is: nums[index] restore the number: index+1.
-    each time we encounter an valid integer, find the correct position and swap, otherwise continue.
-            Time Complexity: O(n)
-            Space Complxity: O(1)
-     */
 
     public static void main(String[] args) {
         int[] arr = new int[]{1, 2, 0};
         System.out.println(firstMissingPositive(arr));
     }
 
+    /*
+        1. Check if 1 is present in array if not return 1
+        2. if nums = [1] the answer is 2
+        3. Replace negative numbers, zeros and numbers larger than n by 1s.
+        4. Walk along array. Change the sign of a-th element if you meet number a. Be careful with duplicates
+           do sign change only once.Use index 0 to save an information about presence of number n since index n is not available.
+        5. Walk again along the array. Return the index of the first positive element.
+        6. If nums[0] > 0 return n.
+        7. If on the previous step you didn't find the positive element in nums, that means that the answer is n + 1.
+
+     Time Complexity: O(n) since we do here is four walks along the array of length N
+     Space Complxity: O(1) since this is constant space solution
+
+     */
     public static int firstMissingPositive(int[] nums) {
-        if (nums == null || nums.length == 0) {
+
+        int n = nums.length;
+
+        // Base case.
+        int contains = 0;
+        for (int i = 0; i < n; i++)
+            if (nums[i] == 1) {
+                contains++;
+                break;
+            }
+
+        if (contains == 0)
             return 1;
+
+        // nums = [1]
+        if (n == 1)
+            return 2;
+
+        // Replace negative numbers, zeros,
+        // and numbers larger than n by 1s.
+        // After this convertion nums will contain
+        // only positive numbers.
+        for (int i = 0; i < n; i++)
+            if ((nums[i] <= 0) || (nums[i] > n))
+                nums[i] = 1;
+
+        // Use index as a hash key and number sign as a presence detector.
+        // For example, if nums[1] is negative that means that number `1`
+        // is present in the array.
+        // If nums[2] is positive - number 2 is missing.
+        for (int i = 0; i < n; i++) {
+            int a = Math.abs(nums[i]);
+            // If you meet number a in the array - change the sign of a-th element.
+            // Be careful with duplicates : do it only once.
+            if (a == n)
+                nums[0] = -Math.abs(nums[0]);
+            else
+                nums[a] = -Math.abs(nums[a]);
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            // Swap the number until they are in the right position.
-            while (nums[i] >= 1 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1]) {
-                int temp = nums[nums[i] - 1];
-                nums[nums[i] - 1] = nums[i];
-                nums[i] = temp;
-            }
+        // Now the index of the first positive number
+        // is equal to first missing positive.
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > 0)
+                return i;
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != i + 1) {
-                return i + 1;
-            }
-        }
-        return nums.length + 1;
+        if (nums[0] > 0)
+            return n;
+
+        return n + 1;
     }
 
 }
