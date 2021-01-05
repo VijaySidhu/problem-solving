91654e707c68cbd07b617e652864f763f12d4542
Vijay Sidhu <vijaysidhu@gmail.com>
2021-01-05 13:34:26 -0800

    coin change

---
+++ src/main/java/com/problem/solving/algorithms/arrays/MaxSumIncreasingSubsequence.java
@@ -0,0 +1,35 @@
+package com.problem.solving.algorithms.arrays;
+
+import java.util.Arrays;
+
+public class MaxSumIncreasingSubsequence {
+    public static void main(String[] args) {
+        int[] input = new int[]{1, 2, 3};
+        System.out.println(sumOfLIS(input));
+    }
+
+
+    public static int sumOfLIS(int[] arr) {
+        int T[] = new int[arr.length];
+
+        for (int i = 0; i < T.length; i++) {
+            T[i] = arr[i];
+        }
+
+        for (int i = 1; i < T.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    T[i] = Math.max(T[i], T[j] + arr[i]);
+                }
+            }
+        }
+
+        int max = T[0];
+        for (int i = 1; i < T.length; i++) {
+            if (T[i] > max) {
+                max = T[i];
+            }
+        }
+        return max;
+    }
+}
