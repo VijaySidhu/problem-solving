801a59fe1760c391ea66b65e1a237fe3e7b6d0ae
Vijay Sidhu <vijaysidhu@gmail.com>
2021-01-05 12:52:49 -0800

    coin change

--- src/main/java/com/problem/solving/algorithms/arrays/CoinChangeMin.java
+++ src/main/java/com/problem/solving/algorithms/arrays/CoinChangeMin.java
@@ -1,10 +1,14 @@
 package com.problem.solving.algorithms.arrays;
 
 import java.util.Arrays;
-
+/*
+You are given coins of different denominations and a total amount of money amount. Write a function to compute the
+fewest number of coins that you need to make up that amount.
+If that amount of money cannot be made up by any combination of the coins, return -1.
+ */
 public class CoinChangeMin {
 
-    // We are going over every single coin and amount so TC O(mXn)
+    // We are going over every single coin and amount so TC O(mXn) Sc O(d)
     public static int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
         // Fill an array with invalid amount basically value greater than amount
@@ -14,6 +18,7 @@ public class CoinChangeMin {
             for (int j = 0; j < coins.length; j++) {
                 // Check if current coin is acceptable not greater the current amout
                 if (coins[j] <= i) {
+                    // Back Track and take min value
                     dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
                 }
             }
