b58650ce9209bfecc618f0bef7caa954160624ff
Vijay Sidhu <vijaysidhu@gmail.com>
2021-01-11 11:51:09 -0800

    detect cycle in undirected graph recursive working

--- src/main/java/com/problem/solving/algorithms/graphs/adjacencylist/DetectCycleDirectedG.java
+++ src/main/java/com/problem/solving/algorithms/graphs/adjacencylist/DetectCycleDirectedG.java
@@ -5,30 +5,77 @@ import java.util.*;
 public class DetectCycleDirectedG {
 
     public static void main(String[] args) {
+        // Yes in below there is cycle
         int[][] edges = new int[][]{
                 {0, 1},
+                {1, 0},
                 {1, 2},
+                {2, 1},
                 {2, 0},
+                {0, 2},
+                {3, 2},
+                {2, 3}
+
         };
-        Graph g = new Graph(3, edges);
+        int[][] noCycle = new int[][]{
+                {0, 1},
+                {1, 0},
+                {1, 2},
+                {2, 1},
+                {3, 2},
+                {2, 3}
+
+        };
+        Graph g = new Graph(4, noCycle);
         Map<Integer, List<Integer>> graph = g.graph;
         Set<Integer> seen = new HashSet<>();
         int source = 0;
-        System.out.println(dfs(source, graph, seen));
+
+        // Recursive
+        System.out.println(dfs(source, graph, seen, -1));
+
+        // Iterative solution
+        Stack<Integer> stack = new Stack<>();
+        stack.push(source);
+        seen = new HashSet<>();
+        seen.add(source);
+
+        System.out.println(iterativeDFS(stack, graph, seen));
 
     }
 
-    private static boolean dfs(int current, Map<Integer, List<Integer>> graph, Set<Integer> seen) {
-        if (seen.contains(current)) {
-            return true;
-        }
-        List<Integer> neighborus = graph.get(current);
-        for (int n : neighborus) {
-            if (!seen.contains(current)) {
-                seen.add(current);
-                if (dfs(n, graph, seen)) {
+    public static boolean iterativeDFS(Stack<Integer> stack, Map<Integer, List<Integer>> graph, Set<Integer> seen) {
+        while (!stack.isEmpty()) {
+            int parent = 0;
+            int current = stack.pop();
+            parent = current;
+
+            List<Integer> neighbors = graph.get(current);
+            for (int i : neighbors) {
+                if (!seen.contains(i)) {
+                    stack.push(i);
+                    seen.add(i);
+                } else if (i != parent) {
                     return true;
                 }
+            }
+        }
+
+        return false;
+
+    }
+
+    private static boolean dfs(int current, Map<Integer, List<Integer>> graph, Set<Integer> seen, int parent) {
+
+        List<Integer> neighborus = graph.get(current);
+        for (int n : neighborus) {
+            if (!seen.contains(n)) {
+                seen.add(n);
+                if (dfs(n, graph, seen, current)) {
+                    return true;
+                }
+            } else if (n != parent) {
+                return true;
             }
 
         }
