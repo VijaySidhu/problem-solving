4c809aaa91a6cebe0d091bcf9b2a4b1238b4bf5d
Vijay Sidhu <vijaysidhu@gmail.com>
2021-01-12 19:17:35 -0800

    unique path 2 with obstacles

--- src/main/java/com/problem/solving/algorithms/arrays/DriverLessCar.java
+++ src/main/java/com/problem/solving/algorithms/arrays/DriverLessCar.java
@@ -9,24 +9,31 @@ public class DriverLessCar {
         System.out.println(carTrack(5, 5));
     }
 
+
     public static int carTrack(int m, int n) {
-        int[][] dp = new int[m][n];
+        int[][] dp = new int[m + 1][n + 1];
 
         // Fill an array with 1
         for (int[] arr : dp) {
-            Arrays.fill(arr, 1);
+            Arrays.fill(arr, 0);
         }
 
+        System.out.println(Arrays.toString(dp));
+
+        dp[1][1] = 1;
+
         // Iterate over matrix
-        for (int r = 1; r < n; r++) {
-            for (int c = 1; c < m; c++) {
+        for (int r = 0; r <= m; r++) {
+            for (int c = 1; c <= n; c++) {
                 if (r < c) {
                     continue;
                 }
-                dp[r][c] = dp[r - 1][c] + dp[r][c - 1];
+
+                if (r + 1 <= m) dp[r + 1][c] += dp[r][c];
+                if (c + 1 <= n) dp[r][c + 1] += dp[r][c];
             }
         }
-        return dp[m - 1][n - 1];
+        return dp[m][n];
 
     }
 }
