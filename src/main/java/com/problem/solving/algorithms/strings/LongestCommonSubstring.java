d5366dfdf967a00ca944a8d036614c288c35c890
Vijay Sidhu <vijaysidhu@gmail.com>
2021-01-19 17:49:20 -0800

    longest common substring length

---
+++ src/main/java/com/problem/solving/algorithms/strings/LongestCommonSubstring.java
@@ -0,0 +1,22 @@
+package com.problem.solving.algorithms.strings;
+
+public class LongestCommonSubstring {
+
+    public static int findLength(int[] A, int[] B) {
+        int ans = 0;
+        int[][] memo = new int[A.length + 1][B.length + 1];
+        for (int i = A.length - 1; i >= 0; --i) {
+            for (int j = B.length - 1; j >= 0; --j) {
+                if (A[i] == B[j]) {
+                    memo[i][j] = memo[i + 1][j + 1] + 1;
+                    if (ans < memo[i][j]) ans = memo[i][j];
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
